5f114e915b5e714e458b8b8224b7450af905ab19
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2024-03-09 21:11:11 -0300

    Update Navbar.tsx

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const tempAwait =()=>{
     return new Promise((resolve)=>{
         setTimeout(()=>{
@@ -11,14 +13,14 @@ export const Navbar = async() => {
     console.log('render navbar');
 
     await tempAwait();
-    
+
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
       <span>Home</span>
       <div className="flex flex-1"></div>
-      <a className="mr-2" href="about">About</a>
-      <a className="mr-2" href="pricing">Pricing</a>
-      <a className="mr-2" href="contact">Contact</a>
+      <Link className="mr-2" href="about">About</Link>
+      <Link className="mr-2" href="pricing">Pricing</Link>
+      <Link className="mr-2" href="contact">Contact</Link>
     </nav>
   )
 }
